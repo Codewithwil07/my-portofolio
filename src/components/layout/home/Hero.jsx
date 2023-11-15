7195e59bb90d1956ecf4bc41be86c6652722622e
wilywangceo@gmail.com <wilywangceo@gmail.com>
2023-11-15 21:47:08 +0700

    update Projects.jsx

--- src/components/layout/home/Hero.jsx
+++ src/components/layout/home/Hero.jsx
@@ -8,17 +8,35 @@ export default function Hero() {
                 <p className='text-4xl font-[500] text-gray-400 md:text-4xl relative top-2'>
                     Mern Stack,
                 </p>
-                <h1 className='font-extrabold text-4xl md:text-4xl' id='darkModeTheme'>
+                <h1
+                    className='font-extrabold text-4xl md:text-4xl'
+                    id='darkModeTheme'
+                >
                     Frontend Developer
                 </h1>
-                <img
-                    src={image.Coretan}
-                    alt=''
-                    className='w-60'
-                />
+                <svg
+                    className='relative bottom-8 right-0'
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='300'
+                    height='100'
+                    viewBox='0 0 1267 124'
+                    fill='none'
+                >
+                    <path
+                        d='M7 117C317.036 69.3021 759.72 15.0212 1139.79 6.16285C1140.95 6.13577 1141.18 7.77534 1140.07 8.11441L862.24 92.8758C861.079 93.2302 861.435 94.9594 862.642 94.8263L1260 51'
+                        stroke='#007BFF'
+                        stroke-width='12'
+                        stroke-linecap='square'
+                    />
+                </svg>
             </div>
-            <p className='text-md md:text-base' id='darkModeTheme'>Specified In</p>
-            <p className='text-md font-semibold md:text-base' id='darkModeTheme'>
+            <p className='text-md md:text-base' id='darkModeTheme'>
+                Specified In
+            </p>
+            <p
+                className='text-md font-semibold md:text-base'
+                id='darkModeTheme'
+            >
                 Website & Webpages
             </p>
             <Button.Rounded />
--- src/components/layout/home/Projects.jsx
+++ src/components/layout/home/Projects.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 import { image } from '../../../constant';
 import Card from '../../element/Card/Card';
 import Button from '../../element/Button/Button';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 const project = [
     {
@@ -16,10 +18,35 @@ const project = [
 ];
 
 const Projects = () => {
+    const control = useAnimation();
+    const [ref, inView] = useInView();
+
+    React.useEffect(() => {
+        if (inView) {
+            control.start({ opacity: 1, x: 0 });
+        }
+    }, [control, inView]);
     return (
         <section className='mx-auto py-20 flex flex-col gap-y-10'>
             <div className='flex flex-col items-center gap-y-5'>
-                <img src={image.MiniWave1} alt='' className='w-72' />
+                <motion.svg
+                    ref={ref}
+                    initial={{ opacity: 0, left: 2 }}
+                    animate={control}
+                    transition={{ duration: 1 }}
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='300'
+                    height='108'
+                    viewBox='0 0 1319 108'
+                    fill='none'
+                >
+                    <path
+                        d='M20.2121 87.1275L147.489 37.3605C162.124 31.6379 178.231 41.2362 180.163 56.8313V56.8313C182.184 73.1454 199.593 82.686 214.432 75.6115L284.401 42.2526C299.878 34.8738 318.348 42.5044 324.129 58.6462V58.6462C324.144 58.6867 324.154 58.7284 324.162 58.7709V58.7709C326.212 70.9379 337.919 78.9984 350.017 76.5725L362.962 73.9766L438.23 50.7072C453.12 46.1038 468.347 42.6677 483.771 40.4302L490.242 39.4915C522.589 34.7992 555.485 35.4246 587.631 41.3429L615.628 46.4976L692.235 53.6392C725.114 56.7042 758.273 54.3201 790.375 46.5828L831.918 36.5701C874.207 26.3777 918.206 25.5036 960.866 34.0083L1008.32 43.4691C1044.24 50.6294 1081.16 51.1529 1117.27 45.0137L1209.13 29.3935L1298.12 20.1437'
+                        stroke='#007BFF'
+                        stroke-width='40'
+                        stroke-linecap='round'
+                    />
+                </motion.svg>
                 <h1
                     className='font-bold text-3xl text-center'
                     id='darkModeTheme'
